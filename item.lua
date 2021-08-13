ae7e844abf57ee23b211459efc3f646ca95001b2
shakiata <80846748+shakiata@users.noreply.github.com>
2021-08-13 14:26:34 -0400

    var fixes to syntax

--- item.lua
+++ item.lua
@@ -1,20 +1,31 @@
 --item.lua
 local spidertron2 = table.deepcopy(data.raw["spider-vehicle"]["spidertron"])
 
-spidertron2.name = "spidertron-2"
+spidertron2.name = "spidertron2"
 spidertron2.icons = {
     {
         icon = spidertron2.icon,
         tint = {r=1,g=0.5,b=1,a=0.3}
     },
 }
-spidertron2.equipment_grid = "65"
+spidertron2.equipment_grid = "large-equipment-grid"
 
 
-local recipe = tabel.deepcopy(data.raw["recipe"]["spidertron"])
+local recipe = table.deepcopy(data.raw["recipe"]["spidertron"])
+
 recipe.enabled = true
-recipe.name = "Spidertron-2"
-recipe.ingredients = {{"copper-plate",1000},{"steel-plate",750}{"raw-fish",50}{"roboport",10}}
-recipe.result = "spidertron-2"
+recipe.name = "spidertron2"
+    recipe.ingredients = {
+    {"exoskeleton-equipment", 8},
+    {"fusion-reactor-equipment", 4},
+    {"rocket-launcher", 4},
+    {"rocket-control-unit", 26},
+    {"low-density-structure", 250},
+    {"radar", 2},
+    {"effectivity-module-3", 2},
+    {"raw-fish", 25}
+}
+
+recipe.result = "spidertron2"
 
 data:extend{spidertron2,recipe}
