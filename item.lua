162daa71388834b033fa95afdf695aea1ab9d392
shakiata <80846748+shakiata@users.noreply.github.com>
2021-08-14 11:39:10 -0400

    working with set grid
    need to get icon working still and bump speed

--- item.lua
+++ item.lua
@@ -1,6 +1,6 @@
 --item.lua
 local spidertron2 = table.deepcopy(data.raw["spider-vehicle"]["spidertron"])
-
+-- this should copy the part that is in your invitory
 
 spidertron2.name = "spidertron-placed"
 spidertron2.icons = {
@@ -10,18 +10,19 @@ spidertron2.icons = {
     },
 }
 local spidertron2e = table.deepcopy(data.raw["item-with-entity-data"]["spidertron"])
-
+-- this should copy the part that gets placed when you take from invintory and place
+--on ground,   it should also edit the equipment grid
 spidertron2e.name = "spidertron-placed"
 
-spidertron2e.equipment_grid = "large-equipment-grid"
+spidertron2.equipment_grid = "large-equipment-grid"
 
-spidertron2.place_result = "spidertron-placed"
+spidertron2e.place_result = "spidertron-placed"
 
 
 local recipe = table.deepcopy(data.raw["recipe"]["spidertron"])
-
+-- copies recipe and changes it for the new one.... This actually works 
 recipe.enabled = true
-recipe.name = "spidertron-2"
+recipe.name = "spidertron-placed"
     recipe.ingredients = {
     {"exoskeleton-equipment", 8},
     {"fusion-reactor-equipment", 4},
